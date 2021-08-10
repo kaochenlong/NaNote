f26688118dc7a102a8c81758397e51e49d596279
eddie <eddie@5xruby.tw>
2021-08-10 15:41:47 +0800

    add validation to Note

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,21 @@
+class NotesController < ApplicationController
+	def index
+	end
+
+	def new
+		@note = Note.new
+	end
+
+	def create
+		# Strong Parameter
+		clean_note = params.require(:note).permit(:title, :content)
+		@note = Note.new(clean_note)
+
+		if @note.save
+			redirect_to "/notes"
+		else
+			## 待處理
+			render :new
+		end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+	resources :notes
+
 	get "/hello", to: "pages#main"
 	get "/about", to: "pages#about"
-
 	get "/users", to: "users#profile"
 end
