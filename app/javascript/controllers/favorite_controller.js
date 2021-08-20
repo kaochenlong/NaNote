f23f790198a59ef37efc5ea00721575a3eedb9e0
eddie <eddie@5xruby.tw>
2021-08-20 14:02:08 +0800

    WIP - refactor with stimulus

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -1,8 +1,33 @@
 import { Controller } from "stimulus";
+// import ax from "axios";
+import ax from "lib/http/ax";
+
+function addFavorite(id) {
+  const url = `/api/v1/notes/${id}/favorite`;
+
+  ax.post(url)
+    .then((res) => {
+      const icon = document.querySelector(".favorite_icon");
+
+      if (res.data.status === "added") {
+        icon.classList.remove("favorite-off");
+        icon.classList.add("favorite-on");
+      } else {
+        icon.classList.remove("favorite-on");
+        icon.classList.add("favorite-off");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 export default class extends Controller {
+  static targets = ["button"];
+  static values = { id: Number };
+
   toggleFavorite(e) {
     e.preventDefault();
-    console.log(123);
+    addFavorite(this.idValue);
   }
 }
